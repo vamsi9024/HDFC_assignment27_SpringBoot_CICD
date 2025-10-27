2a2fb96066ec0716718f7850bfc488e81ff01eab
vamsi9024 <petavamsikrishna9024@gmail.com>
2025-10-27 23:23:59 +0530

    commited

---
+++ src/main/java/com/example/SpringBoot_cicd/SpringBootCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCicdApplication.class, args);
+	}
+
+}
